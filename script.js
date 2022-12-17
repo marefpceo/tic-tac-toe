a680f24f7b049d8c85eccf3bbcdc26bde3549e44
marefpceo <marefpceo@gmail.com>
2022-12-17 09:34:05 -0800

    Stopping point for working on game win logic

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const Gameboard = (() => {
-  const gameboard = ['','','','','','','','',''];
+  const gameboard = [];
   const board = document.getElementById('board');
 
   const drawGrid = () => {
     for(let i = 0; i < 9; i += 1){
       const div = document.createElement('div');
-      const value = document.createTextNode(gameboard[i]);
-      div.appendChild(value);
-      board.appendChild(div); 
+      div.setAttribute('id',`div-${i}`);
+      gameboard.push(board.appendChild(div)); 
     }
   }
 
@@ -37,39 +36,74 @@ const Gameboard = (() => {
 
 
 const Player = (name) => {
-  // const {board} = Gameboard;
-
   const getName = () => name;
+
+  const gamesWon = 0; 
 
   const {addGamePiece} = Gameboard;
 
-  return {getName, addGamePiece};
+  return {getName, addGamePiece, gamesWon};
 }
 
 
 const GamePlay = (() => {
   const {board} = Gameboard;
+  const {addGamePiece} = Gameboard;
+  const {render} = Gameboard;
+
   let count = 1;
+  const oIndex = [];
+  const xIndex = [];
+
+  const checkWinPattern = (pattern) => {
+    // const validCombo = [['0','1','2'], ['3','4','5']];
+    const result = ['0','1','2'].every(i => pattern.includes(i));
+    console.log(result);
+  }
+
+  const checkWinner = () => {
+    // const result = winCombo.every(i => oIndex.includes(i));
+    checkWinPattern(oIndex);
+    // console.log(oIndex.toString());
+    // console.log(oIndex.includes());
+  }
+
+  // Selects the position chosen by the current player
   const selectPosition = () => {
-    board.addEventListener('click', (e) => {
-      
+    board.addEventListener('click', (e) => {  
       if (e.target.innerHTML !== '') {
         return;
       } 
       e.target.innerHTML = (count % 2) === 0 ? 'O' : 'X';
-      count += 1;
+      const selectedPiece = e.target.innerHTML;
+      const position = e.target.id;
+
+      if ((count % 2) === 0) {
+        e.target.innerHTML = 'O';
+        oIndex.push(position.slice(-1));
+      } else {
+        e.target.innerHTML = 'X';
+        xIndex.push(position.slice(-1));
+      }
+      addGamePiece(selectedPiece, position.slice(-1));      
+      checkWinner();
+      count += 1;    
     });
   }
 
+  const startGame = () => {
+    selectPosition();
+    render();
+  }  
+
   return {
-    selectPosition,
+    startGame,
   };
 })();
 
 
 
-const player1 = Player('Player1');
-const player2 = Player('Player2');
+// const player1 = Player('Player1');
+// const player2 = Player('Player2');
 
-GamePlay.selectPosition();
-Gameboard.render();
+GamePlay.startGame();
