7642750b7fcb12ce31ed40be8007cdfaf02eb7c8
marefpceo <marefpceo@gmail.com>
2022-12-19 18:17:30 -0800

    Completed playerDisplayInfo method

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const Gameboard = (() => {
   const gameboard = [];
   const board = document.getElementById('board');
-  const gameArea = document.getElementById('game-area');
 
-  console.log(gameArea);
   const drawGrid = () => {
     for(let i = 0; i < 9; i += 1){
       const div = document.createElement('div');
@@ -24,19 +22,9 @@ const Gameboard = (() => {
     }
   }
 
-  const displayPlayerInfo = () => {
-    const player1Info = gameArea.firstChild.nextSibling;
-    const player2Info = gameArea.lastChild.previousSibling;
-    player1Info.textContent = 'Test';
-    player2Info.textContent = 'Test 2';
-    console.log(player1Info);
-    console.log(player2Info);
-  }
-
   const render = () => {
     clearGrid();
     drawGrid();
-    displayPlayerInfo();
   }
 
   return {
@@ -49,12 +37,9 @@ const Gameboard = (() => {
 
 const Player = (name) => {
   const getName = () => name;
-
   const gamesWon = 0; 
 
-  const {addGamePiece} = Gameboard;
-
-  return {getName, addGamePiece, gamesWon};
+  return {getName, gamesWon};
 }
 
 
@@ -64,23 +49,29 @@ const GamePlay = (() => {
   const {render} = Gameboard;
   const oIndex = [];
   const xIndex = [];
-
+  
   let count = 1;
   let playerCount = 0;
 
-  const createPlayer = (name) => {
+  const playerName = (name) => {
     if (name === '' || name === undefined) {
       playerCount += 1;
       this.name = `Player ${playerCount}`;
-    }
+    } else {
+      this.name = name;
+    }    
     return this.name;
   }
+
+  const player1 = Player(playerName());
+  const player2 = Player(playerName());
+
 
   const checkWinPattern = (pattern) => {
     const validCombo = [['0','1','2'], ['3','4','5'], ['6','7','8'], ['0','3','6'],
                         ['1','4','7'], ['2','5','8'], ['0','4','8'], ['2','4','6']];
-
     let result;
+
     for (let i = 0; i < validCombo.length; i += 1){
      result = validCombo[i].every(v => pattern.includes(v));
      if (result === true){
@@ -90,14 +81,35 @@ const GamePlay = (() => {
     return result;
   }
 
-  const checkWinner = () => {
-    checkWinPattern(oIndex);
-    checkWinPattern(xIndex);
+  const displayPlayerInfo = () => {
+    const gameArea = document.getElementById('game-area');
+    const player1Info = gameArea.querySelector('div > h3');
+    const player2Info = gameArea.querySelector('div:nth-child(3) > h3');
 
-    console.log(`O-Index: ${oIndex}`);
-    console.log(`X-Index: ${xIndex}`);
-    console.log(checkWinPattern(oIndex));
-    console.log(checkWinPattern(xIndex));
+    player1Info.innerHTML = player1.getName();
+    player1Info.nextElementSibling.innerHTML = `Wins: ${player1.gamesWon}`;
+
+    player2Info.innerHTML = player2.getName();
+    player2Info.nextElementSibling.innerHTML = `Wins: ${player2.gamesWon}`;
+  }
+
+  const checkWinner = () => {
+    const oWin = checkWinPattern(oIndex);
+    const xWin = checkWinPattern(xIndex);
+
+    if (oWin === true) {
+      setTimeout(() => {
+        alert(`${player2.getName()} wins`);
+        player2.gamesWon += 1;
+        console.log(player2.gamesWon);
+        render();
+        displayPlayerInfo();
+      });
+    } else if(xWin === true) {
+      console.log('X wins');
+    }
+    console.log(oWin);
+    console.log(xWin);
   }
 
   // Selects the position chosen by the current player
@@ -117,29 +129,24 @@ const GamePlay = (() => {
         e.target.innerHTML = 'X';
         xIndex.push(position.slice(-1));
       }
-      addGamePiece(selectedPiece, position.slice(-1));      
+      addGamePiece(selectedPiece, position.slice(-1));    
       if (count > 4) {
         checkWinner();
       }
       count += 1;    
     });
   }
-
+ 
   const startGame = () => {
-    const player1 = Player(createPlayer());
-    const player2 = Player(createPlayer());
+    displayPlayerInfo();
     selectPosition();
     render();
   }  
 
   return {
     startGame,
+    displayPlayerInfo,
   };
 })();
-
-
-
-// const player1 = Player('Player1');
-// const player2 = Player('Player2');
 
 GamePlay.startGame();
